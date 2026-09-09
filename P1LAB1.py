e4f86373ba2a5c22734192dc0e073cf5ca0e4830
AdamsH-FTCC <adamsh5535@student.faytechcc.edu>
2026-09-09 11:03:33 -0400

    Add files via upload

---
+++ P1LAB1.py
@@ -0,0 +1,14 @@
+# Hannah Adams
+# Spetember, 9, 2026
+# Lab1 - CIT 110
+# An attempt at a call and response program that remembers the user's name and welcomes them to class
+
+# greet user
+print ("What's up gamer🎮🔥? Welcome to python!")
+
+# prompt user for inputs
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+# system outputs
+print("Awesome! Welcome to CIT 110", first_name, last_name + "! You got this!")
