e5a4bd5d4275517657e45566d4aa250a770667a4
AdamsH-FTCC <adamsh5535@student.faytechcc.edu>
2026-09-22 19:30:28 -0400

    Add files via upload
    working with integers for  addition, subtraction, and exponentials

---
+++ P1HW1_AdamsHannah.py
@@ -0,0 +1,23 @@
+# Hannah Adams
+# September, 9, 2026
+# P1 HW1
+# calculations for exponents, addition, and subtraction
+
+print("--------------Exponents--------------")
+print()
+
+#variables
+base = int(input("Enter a base number: "))
+expo = int(input("Enter an exponent number: "))
+result = (base ** expo)
+
+print(base, "raised to the power of", expo, "is", result, "!!")
+
+#addition time!
+print("--------------Addition & Subtraction--------------")
+print()
+num1 = int(input("Enter a starting integer: "))
+num2 = int(input("Enter an integer to add: "))
+num3 = int(input("Enter an integer to subtract: "))
+rezz = num1 + num2 - num3
+print(num1, "+", num2, "-", num3, "is", rezz, "!!")
